b36dc8c34af9c0021e50568add35e0eab9c4f5de
Argjendik <argjend.k@excelentis.org>
2024-12-30 19:27:44 +0100

    Fix bcrypt references and Prisma permissions

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: cd backend && npm install && npx prisma migrate deploy
-web: cd backend && npm install && npm start 
+release: cd backend && npm install --omit=dev && npm install -g prisma && npx prisma generate && npx prisma migrate deploy
+web: cd backend && npm install --omit=dev && npm start 
--- backend/src/create-admin.js
+++ backend/src/create-admin.js
@@ -9,8 +9,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
-    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
@@ -35,47 +35,46 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+var bcrypt = require("bcryptjs");
 var client_1 = require("@prisma/client");
-var bcrypt = require("bcrypt");
+var prisma = new client_1.PrismaClient();
 function createAdmin() {
     return __awaiter(this, void 0, void 0, function () {
-        var prisma, hashedPassword, admin, error_1;
+        var hashedPassword, admin;
         return __generator(this, function (_a) {
             switch (_a.label) {
-                case 0:
-                    prisma = new client_1.PrismaClient();
-                    _a.label = 1;
+                case 0: return [4 /*yield*/, bcrypt.hash('Admin123!', 10)];
                 case 1:
-                    _a.trys.push([1, 4, 5, 7]);
-                    return [4 /*yield*/, bcrypt.hash('Admin123!', 10)];
-                case 2:
                     hashedPassword = _a.sent();
-                    return [4 /*yield*/, prisma.user.upsert({
-                            where: { email: 'admin@example.com' },
-                            update: {},
-                            create: {
-                                email: 'admin@example.com',
+                    return [4 /*yield*/, prisma.user.create({
+                            data: {
+                                email: 'admin@admin.com',
                                 password: hashedPassword,
-                                name: 'System Admin',
-                                role: 'ADMIN'
+                                role: 'ADMIN',
+                                firstName: 'Admin',
+                                lastName: 'Admin',
                             },
                         })];
-                case 3:
+                case 2:
                     admin = _a.sent();
                     console.log('Admin user created:', admin);
-                    return [3 /*break*/, 7];
-                case 4:
-                    error_1 = _a.sent();
-                    console.error('Error creating admin:', error_1);
-                    return [3 /*break*/, 7];
-                case 5: return [4 /*yield*/, prisma.$disconnect()];
-                case 6:
-                    _a.sent();
-                    return [7 /*endfinally*/];
-                case 7: return [2 /*return*/];
+                    return [2 /*return*/];
             }
         });
     });
 }
-createAdmin();
+createAdmin()
+    .catch(function (e) {
+    console.error(e);
+    process.exit(1);
+})
+    .finally(function () { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, prisma.$disconnect()];
+            case 1:
+                _a.sent();
+                return [2 /*return*/];
+        }
+    });
+}); });
